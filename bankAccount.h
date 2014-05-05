3b7bcd04c29f6fca29db59ce5ac515292d6d4960
3barney <brnbsjm@gmail.com>
2014-05-05 10:49:00 +0300

    the header file that contains the function prototypes

---
+++ bankAccount.h
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class BankAccount
+{
+public:
+	BankAccount (int);
+	int getAccountBalance();
+	void credit(int credit);
+	void debit(int debit);
+
+private:
+	int accountBalance;
+};
